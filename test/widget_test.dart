744378b23891e7808c39c1aacd100e98e5822150
Hinata-hype <jawkittimaporn18@gmail.com>
2026-02-05 13:24:35 +0700

    fix: update package name and resolve test errors

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dev_test_2/main.dart';
+import 'package:dev_test_8/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
